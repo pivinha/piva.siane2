e0f032fa7b0cee1f67cd2e384437020018128737
pivinha <113192680+pivinha@users.noreply.github.com>
2023-10-25 12:07:07 -0300

    Add files via upload

--- tabuada.js
+++ tabuada.js
@@ -2,5 +2,13 @@ const frm = document.querySelector("form")
 const resp = document.querySelector("pre")
 
 frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const numero = Number(frm.inNumero.value)
+    let resposta = ""
+    for(let i = i; i <=10; i++){
+        resposta = resposta + numero + "x" i + "=" + (numero = i) + "\n"
+    }
+    resp.innerText = resposta
     
 })
