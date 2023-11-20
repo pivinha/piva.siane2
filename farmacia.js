eb0ed4f1f185b7c2132d9373930c66500690d833
pivinha <113192680+pivinha@users.noreply.github.com>
2023-11-20 12:28:35 -0300

    Add files via upload

--- farmacia.js
+++ farmacia.js
@@ -2,10 +2,6 @@ const frm = document.querySelector("form")
 const resp1 = document.querySelector("#outResp1")
 const resp2 = document.querySelector("#outResp2")
 
-
-
-
-
 frm.addEventListener("submit", (e) =>{
     e.preventDefault()
 
